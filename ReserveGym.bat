e62f273ecada24fb7bdcca684f182ab99749cf5e
Ryan Dodds <rcdodds@gmail.com>
2020-08-24 20:14:50 -0400

    Adding .bat to execute .py

---
+++ ReserveGym.bat
@@ -0,0 +1,8 @@
+:: Turn off command logging
+ECHO off
+
+:: Run python script to get RV Relocation Deals
+"C:\Users\rcdodds\Documents\GitHub\GymReservation\venv\Scripts\python.exe" "C:\Users\rcdodds\Documents\GitHub\GymReservation\ReserveGym.py"
+
+:: Pause so CMD doesn't close
+PAUSE
