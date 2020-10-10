7156909c6950db9632f848b91a5c38e06724ee0f
Ryan Dodds <rcdodds@gmail.com>
2020-10-10 15:26:26 -0400

    Add text notifications

--- ReserveGym.py
+++ ReserveGym.py
@@ -6,10 +6,15 @@ import json
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-from GoogleCalendar import check_gcal_events, change_gcal_event_title
+from GoogleCalendar import check_gcal_events, change_gcal_event_title   # Custom import
+from TwilioTexts import send_text   # Custom import
 
 
 # Open a Selenium browser while printing status updates. Return said browser for use in scraping.
@@ -42,13 +47,30 @@ def open_gym_scheduler():
 def read_login(filename):
     with open(filename, mode='r') as file:
         login_dict = json.load(file)
-    return login_dict["username"], login_dict["password"]
+    return login_dict['username'], login_dict['password']
+
+
+# Wait for page to load (if required)
+def wait_page_load(drv):
+    # Variables
+    short_timeout = 5                   # Wait a short amount of time for page loading element to appear
+    long_timeout = 60                   # Wait a longer time for page loading element to disappear
+    page_loading_id = 'page_loading'    # ID of page loading element
+
+    try:
+        # Wait for page loading element to appear
+        WebDriverWait(drv, short_timeout).until(ec.presence_of_element_located((By.ID, 'page_loading')))
+        # Wait for page loading element to disappear
+        WebDriverWait(drv, long_timeout).until_not(ec.presence_of_element_located((By.ID, page_loading_id)))
+    except TimeoutException:
+        pass
+
+    # Wait a second then let program resume
+    time.sleep(1)
 
 
 # Submit Lyon Place gym reservation
-def schedule_gym_time(start_date, start_time, duration):
-    # Number of seconds to sleep after
-    sleeptime = 5
+def schedule_gym_time(start_datetime, duration):
 
     # Open website and get to reservations page
     driver = open_gym_scheduler()
@@ -56,51 +78,58 @@ def schedule_gym_time(start_date, start_time, duration):
     # Choose gym
     driver.find_element_by_id('ResourceId').send_keys('Fitness Center Lyon Place')
 
+    # Check Waitlist box. This stops the website from trying to dynamically show available timeslots.
+    driver.find_element_by_id('Waitlist').click()
+    wait_page_load(driver)
+
     # Choose date
-    start_date_str = start_date.strftime("%m/%d/%y")
-    reservation_month_day = int(start_date.strftime("%d"))              # Reservation day of the month (1 - 31)
+    reservation_month_day = int(start_datetime.strftime("%d"))          # Reservation day of the month (1 - 31)
     today_month_day = int(datetime.date.today().strftime("%d"))         # Today's day of the month (1 - 31)
     driver.find_element_by_id('StartDate').click()                      # Open date picker
-    if reservation_month_day < today_month_day:         # If reserving a slot in the next month, move to next month
+    if reservation_month_day < today_month_day:         # If the date is in the next month, move to next month
         driver.find_element_by_class_name('ui-datepicker-next.ui-corner-all').click()
     cells = driver.find_elements_by_xpath('//td')
     dates = [date.text for date in cells]
     cells[dates.index(str(reservation_month_day))].click()              # Pick reservation date
+    wait_page_load(driver)
 
     # Choose duration
     driver.find_element_by_id('Duration').click()
+    wait_page_load(driver)
     driver.find_element_by_xpath("//option[@value='" + str(duration) + "']").click()
-    time.sleep(sleeptime)
+    wait_page_load(driver)
     driver.find_element_by_id('divReservationRequestAdd').click()
+    wait_page_load(driver)
 
     # Choose start time
-    start_time_str = start_time.strftime("%I:%M %p")
-    start_time_hour = str(int(start_time.strftime("%I")))
-    start_time_mins = str(int(start_time.strftime("%M")))
-    start_time_ampm = start_time.strftime("%p")
+    start_time_hour = str(int(start_datetime.strftime("%I")))
+    start_time_mins = str(int(start_datetime.strftime("%M")))
+    start_time_ampm = start_datetime.strftime("%p")
     driver.find_element_by_id('HoursStart').send_keys(start_time_hour)
-    time.sleep(sleeptime)
+    wait_page_load(driver)
     driver.find_element_by_id('MinutesStart').send_keys(start_time_mins)
-    time.sleep(sleeptime)
+    wait_page_load(driver)
     driver.find_element_by_id('AmPmStart').send_keys(start_time_ampm)
-    time.sleep(sleeptime)
+    wait_page_load(driver)
 
-    # Click create reservation button
-    driver.find_element_by_id('btnCreateReservation').click()
-    time.sleep(sleeptime)
-    # Check that reservation is being created at the desired time
-    desired_start_time = start_date_str + ' ' + start_time_str
-    actual_start_time = driver.find_element_by_class_name('span9').text
-    if actual_start_time == desired_start_time:
-        # Confirm reservation
-        driver.find_element_by_id('btnPayNow').click()
-        time.sleep(sleeptime)
-        print('Reservation created for ' + actual_start_time)
-        driver.switch_to.alert.dismiss()
-        event_created = True
-    else:
-        print('Reservation could not be submitted for ' + desired_start_time)
-        event_created = False
+    # If the wait list warning is not present, submit the reservation
+    event_created = False
+    if not driver.find_element_by_id('lblTimeError').is_displayed():
+        # Click create reservation button
+        driver.find_element_by_id('btnCreateReservation').click()
+        wait_page_load(driver)
+        # Check that reservation is being created at the desired time
+        actual_start_str = driver.find_element_by_class_name('span9').text
+        actual_start_dt = datetime.datetime.strptime(actual_start_str, '%m/%d/%Y %I:%M %p')
+        deviation = max(actual_start_dt, start_datetime) - min(actual_start_dt, start_datetime)
+        if deviation.seconds <= 1.5 * 3600:
+            # Confirm reservation
+            driver.find_element_by_id('btnPayNow').click()
+            time.sleep(5)
+            print('Reservation created for ' + actual_start_dt.strftime('%m/%d/%y %I:%M %p'))
+            driver.switch_to.alert.dismiss()
+            event_created = True
+
     driver.close()
     return event_created
 
@@ -123,17 +152,18 @@ def main():
         # Parse Google Calendar timestamp and split data into necessary pieces
         pending_gcal_datetimes = [datetime.datetime.strptime(dt.replace('-04:00', ''), "%Y-%m-%dT%H:%M:%S")
                                   for dt in pending_gcal_timestamps[pending_event_id]]
-        st_day = pending_gcal_datetimes[0].date()
-        st_time = pending_gcal_datetimes[0].time()
+        start_dt = pending_gcal_datetimes[0]
         dur = int((pending_gcal_datetimes[1] - pending_gcal_datetimes[0]).total_seconds()/60)
 
         # Attempt to create reservation
-        if schedule_gym_time(st_day, st_time, dur):
+        if schedule_gym_time(start_dt, dur):
             # If successful, update Google Calendar event title
             change_gcal_event_title(pending_event_id, gcal_confirmed_event_title)
         else:
-            # Send text notification
-            print('Send a text via Twilio')
+            # Print failure message and send text message via Twilio
+            fail_msg = 'Reservation could not be submitted for ' + start_dt.strftime('%m/%d/%y %I:%M %p')
+            print(fail_msg)
+            send_text('Gym Reservation Failure - ' + fail_msg, '+14847233363')
 
 
 # Let's get it going
